7cb22567cf998f335ae8321ef4bf69003a962c72
Deflandrenassim <78204743+Deflandrenassim@users.noreply.github.com>
2022-09-14 13:41:13 +0200

    feature: add CardImg

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
 import Card from '../components/Card/Card';
+import CardPicture from '../components/Card/CardPicture';
 
 function Api() {
   const [pokemons, setPokemons] = useState();
@@ -15,19 +16,13 @@ function Api() {
     Promise.all(results.map(({ name }) => callApi(name)))
       .then((values) => {
         setPokemons(values);
-      //  console.log(values);
       });
   };
 
   useEffect(() => {
     Axios.get('https://pokeapi.co/api/v2/pokemon?limit=20') // renvoi 20 pokemon
       .then((response) => {
-        // console.log(response);
         getAllPokemonsInfo(response.data.results);
-        // .then((test) => {
-        //   // console.log(response.data.results)
-        //   console.log(test);
-        // });
       });
   }, []);
   return (
@@ -37,9 +32,9 @@ function Api() {
               <ul>
                 {pokemons.map((pokemon) => (
                   <Card key={pokemon.id}>
-                    {pokemon.name}
+                    {/* <img src={pokemon.name} alt="pokemon" /> */}
+                    <CardPicture img={pokemon.sprites.back_default} />
                     {pokemon.base_experience}
-                    {pokemon.sprites.back_shiny}
                   </Card>
                 ))}
               </ul>
---
+++ src/components/Card/CardPicture.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function CardPicture({ img }) {
+  return (
+    <img src={img} alt="" />
+  );
+}
+
+export default CardPicture;
