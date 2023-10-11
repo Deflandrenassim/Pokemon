8f3eb0cc52cbbd0e1a16f3764e33e00f5ed95811
Deflandrenassim <nassim.def@gmail.com>
2023-10-11 21:33:37 +0200

    refacto : rename file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
+import './App.css';
 import React from 'react';
 import Head from './container/Head/Head';
-import PokemonPage from './pages/PokemonPage';
-import './App.css';
+import Home from './pages/home';
 
 function App() {
   return (
     <>
       <Head />
       <div className="App">
-        <PokemonPage />
+        <Home />
       </div>
     </>
   );
