9c8cbea03eb55b33c1376f4788e0e6c02b69e6ac
Deflandrenassim <nassim.def@gmail.com>
2023-10-12 21:49:18 +0200

    regle du jeux

---
+++ src/container/Game/RegleGame.jsx
@@ -0,0 +1,15 @@
+export function RegleGame() {
+    return (
+        <div>
+            <h1> Welcome Game PokeNass</h1>
+            <span>
+                Le But du jeux est de cliquer à tous de role cela fera apparaitre un pokemon au
+                HASARD. Celui qui obtiendra le pokemon le plus fort (Force,Attaque) le plus
+                puissance obtiendra le point.
+            </span>
+            <span>
+                AMUSEZ VOUS BIEN QUE LE MEILLEUR DRESSEUR GAGNE
+            </span>
+        </div>
+    )
+}
