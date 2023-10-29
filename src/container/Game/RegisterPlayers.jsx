ffaaca63e2d4469c81e82f2fb8102e8031f7e02a
Deflandrenassim <nassim.def@gmail.com>
2023-10-29 07:48:09 +0100

    feat : register players visible

--- src/container/Game/RegisterPlayers.jsx
+++ src/container/Game/RegisterPlayers.jsx
@@ -1,25 +1,13 @@
 import { useState } from 'react';
-import { Input } from '../../components/Input/Input';
-import { Button } from '../../components/Button/Button';
 import { RegleGame } from './RegleGame';
-import { NamePlayers } from './NamePlayers';
+import { ValidePlayers } from './ValidePlayers';
 
 export function RegisterPlayers() {
-    const [playerOne, setPlayerOne] = useState('');
-    const [playerTwo, setPlayerTwo] = useState('');
-    const [displayName, setDisplayName] = useState(false);
+
     const [modalActive, setModalActive] = useState(true);
-
-    function handleValidRegister() {
-        setDisplayName(!displayName)
-    }
-
     return (
         <>
-            <NamePlayers displayName={displayName} playerOne={playerOne} playerTwo={playerTwo} />
-            <Input type="text" placeholder="Joueur 1" onChange={(e) => setPlayerOne(e.target.value)} />
-            <Input type="text" placeholder="Joueur 2" onChange={(e) => setPlayerTwo(e.target.value)} />
-            <Button onClick={() => handleValidRegister()}> Valider </Button>
+            <ValidePlayers />
             <RegleGame modalActive={modalActive} setModalActive={setModalActive} />
         </>
     )
---
+++ src/container/Game/ValidePlayers.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import { Button } from '../../components/Button/Button';
+import { NamePlayers } from './NamePlayers';
+import { Input } from '../../components/Input/Input';
+
+export function ValidePlayers() {
+    const [playerOne, setPlayerOne] = useState('');
+    const [playerTwo, setPlayerTwo] = useState('');
+    const [displayName, setDisplayName] = useState(false);
+    const [showInput, setShowInput] = useState(true);
+
+    function handleValidRegister() {
+        setDisplayName(!displayName)
+        setShowInput(!showInput);
+        console.log(showInput);
+    }
+
+    return (
+        <div>
+            {showInput ? (
+
+                <div>
+                    <NamePlayers displayName={displayName} playerOne={playerOne} playerTwo={playerTwo} />
+                    <Input type="text" placeholder="Joueur 1" onChange={(e) => setPlayerOne(e.target.value)} />
+                    <Input type="text" placeholder="Joueur 2" onChange={(e) => setPlayerTwo(e.target.value)} />
+                    <Button onClick={() => handleValidRegister()}> Valider </Button>
+                </div>
+            ) : (
+
+                <NamePlayers displayName={displayName} playerOne={playerOne} playerTwo={playerTwo} />
+            )}
+        </div>
+    )
+}
