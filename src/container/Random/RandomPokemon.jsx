a0d2ff9098fcf78dee4765db56a5abcec7a0ddd8
Deflandrenassim <nassim.def@gmail.com>
2023-11-15 23:08:09 +0100

    feat : random

--- src/container/Random/RandomPokemon.jsx
+++ src/container/Random/RandomPokemon.jsx
@@ -8,16 +8,21 @@ export function RandomPokemon({ display }) {
     const allPokemons = useGetAllPokemons();
     const [randomOne, setRandomOne] = useState();
     const [randomTwo, setRandomTwo] = useState();
-
+    const [btn1Disabled, setBtn1Disabled] = useState(true);
+    const [btn2Disabled, setBtn2Disabled] = useState(false);
     function handleRandomOne() {
         const res = Math.floor(Math.random() * allPokemons.length - 1);
         setRandomOne(allPokemons[res]);
+        setBtn1Disabled(!btn1Disabled)
+        setBtn2Disabled(!btn2Disabled)
         console.log('random', randomOne);
 
     }
 
     function handleRandomTwo() {
         const res = Math.floor(Math.random() * allPokemons.length - 1);
+        setBtn1Disabled(!btn1Disabled)
+        setBtn2Disabled(!btn2Disabled)
         setRandomTwo(allPokemons[res])
         console.log('random two', randomTwo)
     }
@@ -27,11 +32,11 @@ export function RandomPokemon({ display }) {
             <div className="randoms-pokemons">
                 <div className="random-one">
                     <CardRandomPokemon random={randomOne} />
-                    <Button onClick={() => handleRandomOne()}> random </Button>
+                    <Button onClick={() => handleRandomOne()} disabled={btn1Disabled}> random </Button>
                 </div>
                 <div className="random-two">
                     <CardRandomPokemon random={randomTwo} />
-                    <Button onClick={() => handleRandomTwo()}> random </Button>
+                    <Button onClick={() => handleRandomTwo()} disabled={btn2Disabled}> random </Button>
                 </div>
             </div>
         )
