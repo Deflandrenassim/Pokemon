3365a05c016c45b82464dd0ba4ca9035d4bcb683
Deflandrenassim <nassim.def@gmail.com>
2023-11-29 22:43:51 +0100

    fix : css point

--- src/container/Game/PlayerPoint.jsx
+++ src/container/Game/PlayerPoint.jsx
@@ -1,12 +1,13 @@
+import './PlayerPoint.css'
 import { useCountPoint } from "../../context/Point"
 
 export function PlayerPoint() {
     const { point } = useCountPoint();
 
     return (
-        <>
+        <div className="points">
             <div> {point.playerPointOne} </div>
             <div>  {point.playerPointTwo} </div>
-        </>
+        </div>
     )
 }
