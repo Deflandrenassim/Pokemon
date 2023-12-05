2241e70514720b7258ff90cbcdf607657216263c
Deflandrenassim <nassim.def@gmail.com>
2023-12-05 21:06:38 +0100

    feature : btn top

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,27 @@
 import './home.css';
 import { Pokemons } from '../container/pokemons/Pokemon';
+import { TbArrowBigUpLine } from "react-icons/tb";
+import { Button } from '../components/Button/Button';
 
 function Home() {
+
+  function goToTop() {
+    console.log("onclick")
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+
 
   return (
     <div className="pokemon_page">
       <div className="pokemon_page_background">
         <Pokemons />
-
+      </div>
+      <div className="home-btn-top">
+        <Button onClick={goToTop}> <TbArrowBigUpLine /> </Button>
       </div>
     </div>
 
